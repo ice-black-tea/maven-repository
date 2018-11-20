1e0dd9dbe31f5e362bfb8c8598c997b4efcd6bdb
HuJi <huji@HuJideiMac.local>
2018-11-20 23:38:09 +0800

    deploy
    Signed-off-by: HuJi <huji@HuJideiMac.local>

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ $# != 1 ];then
+  echo 'deploy argument [snapshot(s for short) | release(r for short) ] needed!'
+  exit 0
+fi
+
+## deploy参数，snapshot 表示快照包，简写为s， release表示正式包，简写为r
+arg=$1
+
+REPOSITORY_PATH="$(dirname $0)"
+
+deploy(){
+  br=$1
+  # 切换对应分支
+  cd $REPOSITORY_PATH && git checkout $br
+  # 开始deploy
+  mvn clean deploy -Dmaven.test.skip  -DaltDeploymentRepository=maven-repo::default::file:$REPOSITORY_PATH/repository
+
+  # deploy 完成,提交
+  cd $REPOSITORY_PATH && git commit -am 'deploy' -s
+  cd $REPOSITORY_PATH && git push origin $br
+
+  # 合并master分支
+  cd $REPOSITORY_PATH && git checkout master
+  cd $REPOSITORY_PATH && git merge $br
+  cd $REPOSITORY_PATH && git commit -am 'merge' -s
+  cd $REPOSITORY_PATH && git push origin master
+}
+
+if [ $arg = 'snapshot' ] || [ $arg = 's' ];then
+  ## 快照包发布
+  cd $REPOSITORY_PATH && deploy snapshot
+elif [ $arg = 'release' ] || [ $arg = 'r' ];then
+  ## 正式包发布
+  cd $REPOSITORY_PATH && deploy release
+else
+  echo 'argument should be snapshot(s for short) or release(r for short). like: `sh deploy.sh snapshot` or `sh deploy.sh s`'
+fi
